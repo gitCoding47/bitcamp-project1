fca676d5e89cdce3a3d3be49198c1a99c3fa4ef6
Nari-Lee <serika9009@gmail.com>
2024-06-27 16:04:26 +0900

    test01

--- app/src/main/java/bitcamp/project1/Test01.java
+++ app/src/main/java/bitcamp/project1/Test01.java
@@ -1,7 +1,15 @@
 package bitcamp.project1;
 
 public class Test01 {
+  void test1() {
+    System.out.println("git Test 입니다.");
+  }
+
   public static void main(String[] args) {
     System.out.println("안녕하세요. 이건입니다.");
+  }
+
+  static void m2() {
+    System.out.println("오호라!");
   }
 }
