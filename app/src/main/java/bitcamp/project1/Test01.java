d50ea11bbdf0c0ddd7c060dd6b65aca916bb70ba
gitCoding47 <goodlife6yr@gmail.com>
2024-06-27 17:21:55 +0900

    merge

--- app/src/main/java/bitcamp/project1/Test01.java
+++ app/src/main/java/bitcamp/project1/Test01.java
@@ -1,11 +1,21 @@
 package bitcamp.project1;
 
 public class Test01 {
+  void test1() {
+    System.out.println("git Test 입니다.");
+  }
+
   public static void main(String[] args) {
     // System.out.println("안녕하세요. 이건입니다.");
+    System.out.println("Ok");
+    System.out.println("test");
+    System.out.println("111");
 
   }
 
+  static void m2() {
+    System.out.println("오호라!");
+  }
   static void m1() {
     System.out.println("오로라");
     System.out.println("Updating 14043d2..ad07275\n" +
@@ -13,5 +23,9 @@ public class Test01 {
         "        app/src/main/java/bitcamp/project1/Test01.java\n" +
         "Please commit your changes or stash them before you merge.\n" +
         "Aborting\n");
+  }
+
+  static void m9() {
+    System.out.println("111");
   }
 }
