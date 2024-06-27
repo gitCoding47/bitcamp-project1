14043d2b3940e969c49bf353ccc38ed8cbac3ca9
gitCoding47 <goodlife6yr@gmail.com>
2024-06-27 16:04:29 +0900

    m1 추가

--- app/src/main/java/bitcamp/project1/Test01.java
+++ app/src/main/java/bitcamp/project1/Test01.java
@@ -2,6 +2,12 @@ package bitcamp.project1;
 
 public class Test01 {
   public static void main(String[] args) {
-    System.out.println("안녕하세요. 이건입니다.");
+    // System.out.println("안녕하세요. 이건입니다.");
+
   }
+
+  static void m1() {
+    System.out.println("오로라");
+  }
+  
 }
