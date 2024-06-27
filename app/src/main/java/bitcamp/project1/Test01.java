622b99f40373a1f641d2405d498d25aad1bbfe4f
Nari-Lee <serika9009@gmail.com>
2024-06-27 16:49:39 +0900

    1

--- app/src/main/java/bitcamp/project1/Test01.java
+++ app/src/main/java/bitcamp/project1/Test01.java
@@ -7,17 +7,19 @@ public class Test01 {
 
   public static void main(String[] args) {
     // System.out.println("안녕하세요. 이건입니다.");
+    System.out.println("Ok");
+    System.out.println("test");
 
   }
 
-<<<<<<< HEAD
   static void m2() {
     System.out.println("오호라!");
   }
-=======
   static void m1() {
     System.out.println("오로라");
   }
-  
->>>>>>> 14043d2b3940e969c49bf353ccc38ed8cbac3ca9
+
+  static void m9() {
+    System.out.println("111");
+  }
 }
