dad168516aa463625d7e22a164e745ed13a6778a
Nari-Lee <serika9009@gmail.com>
2024-06-28 17:19:59 +0900

    카테고리 추가

--- app/src/main/java/bitcamp/project1/App.java
+++ app/src/main/java/bitcamp/project1/App.java
@@ -10,12 +10,9 @@ import bitcamp.project1.vo.AccountBook;
 public class App {
 
   String[] mainMenus = new String[] {"등록", "조회", "삭제", "수정", "도움말", "종료"};
-  String[][] subMenus = {
-      {"예산 설정", "수입 등록", "지출 등록"},
-      {"예산 조회", "가계부 내역 조회"},
-      {"예산 삭제", "가계부 내역 삭제"},
-      {"예산 설정", "수입 수정", "지출 수정"}
-  };
+  String[][] subMenus =
+      {{"예산 설정", "수입 등록", "지출 등록"}, {"예산 조회", "가계부 내역 조회", "카테고리별 내역 조회"}, {"예산 삭제", "가계부 내역 삭제"},
+          {"예산 설정", "수입 수정", "지출 수정"}};
 
   AccountBook accountBook = new AccountBook();
   AppendBoard appendBoard = new AppendBoard(accountBook);
