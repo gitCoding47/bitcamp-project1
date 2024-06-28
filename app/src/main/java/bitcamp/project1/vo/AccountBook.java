13668bc5fa37aa394ee750a101a499b99745d7b7
Nari-Lee <serika9009@gmail.com>
2024-06-28 10:15:37 +0900

    인덱스추가,날짜로 정렬

--- app/src/main/java/bitcamp/project1/vo/AccountBook.java
+++ app/src/main/java/bitcamp/project1/vo/AccountBook.java
@@ -1,12 +1,12 @@
 package bitcamp.project1.vo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class AccountBook {
-
   private int monthlyBudget;
   private ArrayList<Entry> entries = new ArrayList<>();
-  private int nextIndex = 1;
 
   public void setMonthlyBudget(int budget) {
     this.monthlyBudget = budget;
@@ -14,8 +14,14 @@ public class AccountBook {
   }
 
   public void addEntry(Entry entry) {
-    entry.setIndex(nextIndex++);
     entries.add(entry);
+    // 항목을 날짜 순으로 정렬
+    Collections.sort(entries, new Comparator<Entry>() {
+      public int compare(Entry e1, Entry e2) {
+        return e1.getDate().compareTo(e2.getDate());
+      }
+    });
+
     System.out.println("항목이 추가되었습니다: " + entry.getDescription());
   }
 
@@ -23,6 +29,7 @@ public class AccountBook {
     int totalExpenditure = 0;
     int totalIncome = 0;
 
+    // 입력된 값이 지출이면 totalExpenditure , 수입이면 totalIncome에 더해서 저장
     for (Entry entry : entries) {
       if (entry.getType().equals("지출")) {
         totalExpenditure += entry.getAmount();
@@ -30,7 +37,9 @@ public class AccountBook {
         totalIncome += entry.getAmount();
       }
     }
+
     int remainingBudget = monthlyBudget - totalExpenditure;
+
     if (monthlyBudget == 0) {
       System.out.println("예산이 등록되지 않았습니다.");
     } else {
@@ -43,11 +52,14 @@ public class AccountBook {
   public void showEntries() {
     if (entries.isEmpty()) {
       System.out.println("내역이 없습니다.");
+      return;
     }
+
     for (int i = 0; i < entries.size(); i++) {
-      Entry e = entries.get(i);
-      System.out.printf("%d. %s | %s | %d | %s\n", i + 1, e.getIndex(), e.getDescription(),
-          e.getAmount(), e.getDate());
+      entries.get(i).setIndex(i + 1);
+      Entry entry = entries.get(i);
+      System.out.printf("%d. %s | %s | %d | %s\n", entry.getIndex(), entry.getType(),
+          entry.getDescription(), entry.getAmount(), entry.getDate());
     }
   }
 }
