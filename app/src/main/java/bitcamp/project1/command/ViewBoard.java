72016279f57b001b81b9af9079be5f554a2bdabf
Nari-Lee <serika9009@gmail.com>
2024-06-28 10:48:20 +0900

    ViewBoard 리팩토링

--- app/src/main/java/bitcamp/project1/command/ViewBoard.java
+++ app/src/main/java/bitcamp/project1/command/ViewBoard.java
@@ -1,10 +1,11 @@
 package bitcamp.project1.command;
 
 import bitcamp.project1.vo.AccountBook;
+import bitcamp.project1.vo.Entry;
 
 public class ViewBoard {
 
-  AccountBook accountBook;
+  private AccountBook accountBook;
 
   public ViewBoard(AccountBook accountBook) {
     this.accountBook = accountBook;
@@ -13,13 +14,51 @@ public class ViewBoard {
   public void view(String subMenuTitle) {
     switch (subMenuTitle) {
       case "예산 조회":
-        accountBook.showRemainingBudget();
+        showRemainingBudget();
         break;
       case "가계부 내역 조회":
-        accountBook.showEntries();
+        showEntries();
         break;
       default:
         System.out.println("잘못된 메뉴 선택입니다.");
+    }
+  }
+
+  private void showRemainingBudget() {
+    int totalExpenditure = 0;
+    int totalIncome = 0;
+
+    // 입력된 값이 지출이면 totalExpenditure , 수입이면 totalIncome에 더해서 저장
+    for (Entry entry : accountBook.getEntries()) {
+      if (entry.getType().equals("지출")) {
+        totalExpenditure += entry.getAmount();
+      } else if (entry.getType().equals("수입")) {
+        totalIncome += entry.getAmount();
+      }
+    }
+
+    int remainingBudget = accountBook.getMonthlyBudget() - totalExpenditure;
+
+    if (accountBook.getMonthlyBudget() == 0) {
+      System.out.println("예산이 등록되지 않았습니다.");
+    } else {
+      System.out.println("남은 예산: " + remainingBudget);
+    }
+    System.out.println("총 수입: " + totalIncome);
+    System.out.println("총 지출: " + totalExpenditure);
+  }
+
+  public void showEntries() {
+    if (accountBook.getEntries().isEmpty()) {
+      System.out.println("내역이 없습니다.");
+      return;
+    }
+
+    for (int i = 0; i < accountBook.getEntries().size(); i++) {
+      accountBook.getEntries().get(i).setIndex(i + 1);
+      Entry entry = accountBook.getEntries().get(i);
+      System.out.printf("%d. %s | %s | %d | %s\n", entry.getIndex(), entry.getType(),
+          entry.getDescription(), entry.getAmount(), entry.getDate());
     }
   }
 }
--- app/src/main/java/bitcamp/project1/vo/AccountBook.java
+++ app/src/main/java/bitcamp/project1/vo/AccountBook.java
@@ -8,11 +8,6 @@ public class AccountBook {
   private int monthlyBudget;
   private ArrayList<Entry> entries = new ArrayList<>();
 
-  public void setMonthlyBudget(int budget) {
-    this.monthlyBudget = budget;
-    System.out.println("월 예산이 설정되었습니다: " + budget);
-  }
-
   public void addEntry(Entry entry) {
     entries.add(entry);
     // 항목을 날짜 순으로 정렬
@@ -25,41 +20,17 @@ public class AccountBook {
     System.out.println("항목이 추가되었습니다: " + entry.getDescription());
   }
 
-  public void showRemainingBudget() {
-    int totalExpenditure = 0;
-    int totalIncome = 0;
-
-    // 입력된 값이 지출이면 totalExpenditure , 수입이면 totalIncome에 더해서 저장
-    for (Entry entry : entries) {
-      if (entry.getType().equals("지출")) {
-        totalExpenditure += entry.getAmount();
-      } else if (entry.getType().equals("수입")) {
-        totalIncome += entry.getAmount();
-      }
-    }
-
-    int remainingBudget = monthlyBudget - totalExpenditure;
-
-    if (monthlyBudget == 0) {
-      System.out.println("예산이 등록되지 않았습니다.");
-    } else {
-      System.out.println("남은 예산: " + remainingBudget);
-    }
-    System.out.println("총 수입: " + totalIncome);
-    System.out.println("총 지출: " + totalExpenditure);
+  public ArrayList<Entry> getEntries() {
+    return entries;
   }
 
-  public void showEntries() {
-    if (entries.isEmpty()) {
-      System.out.println("내역이 없습니다.");
-      return;
-    }
-
-    for (int i = 0; i < entries.size(); i++) {
-      entries.get(i).setIndex(i + 1);
-      Entry entry = entries.get(i);
-      System.out.printf("%d. %s | %s | %d | %s\n", entry.getIndex(), entry.getType(),
-          entry.getDescription(), entry.getAmount(), entry.getDate());
-    }
+  public int getMonthlyBudget() {
+    return monthlyBudget;
   }
+
+  public void setMonthlyBudget(int budget) {
+    this.monthlyBudget = budget;
+    System.out.println("월 예산이 설정되었습니다: " + budget);
+  }
+
 }
